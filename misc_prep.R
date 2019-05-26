d68d0178944402a06f6415ce31dbedf04d8a6328
Michael Clark <micl@umich.edu>
2019-05-26 14:55:17 -0400

    module 1 slides mostly complete

---
+++ misc_prep.R
@@ -0,0 +1,4 @@
+load('synthetic.RData')
+write_csv(demos_anonymized, 'data/demos_anonymized.csv')
+write_csv(ids_anonymized, 'data/ids_anonymized.csv')
+write_csv(model_variables_anonymized, 'data/model_variables_anonymized.csv')

3c800c5cabac1f61caf4ccc44f3a075e886bdf52
Michael Clark <micl@umich.edu>
2019-05-30 22:55:00 -0400

    add workshop project

---
+++ workshop_project/README.md
@@ -0,0 +1,13 @@
+## Welcome the LLAP Workshop!
+
+This is an RStudio project.  To use it do the following:
+
+1. Open RStudio 
+2. Click File - Open Project 
+3. Navigate to this folder and click the .Rproj file
+
+The only files you need for the workshop are .Rmd notebook files.  You can run
+the code therein as we go along.
+
+To get started run the RunThis.R file.
+
---
+++ workshop_project/RunThis.R
@@ -0,0 +1,13 @@
+# Open me in your RStudio project and run the line below to get started
+
+install.packages('tidyverse')
+
+# IF YOU GET A POPUP/UNDER WINDOW... 
+# Just click ok to allow it to install the packages to a directory you have
+# access to (e.g. 'Documents')
+
+
+
+# Optional
+
+install.packages(c('caret', 'plotly', 'broom', 'ggeffects', 'modelr', 'igraph', 'xgboost'))

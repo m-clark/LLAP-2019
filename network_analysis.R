71d39f72bf50fa40833d1a3520ff43a9b6e10ebd
Michael Clark <micl@umich.edu>
2019-06-03 10:00:59 -0400

    update network files

--- network_analysis.R
+++ network_analysis.R
@@ -5,17 +5,15 @@
 library(igraph) # If you haven't installed this already, then run install.packages("igraph")
 
 # GENERATE 2-MODE NETWORK FOR ALL INVESTIGATORS, 2016-2018
-# Create 2-mode edgelist, then reshape it to a matrix
-alledges <- read.csv("data/all_2mode.csv", header=T, as.is=T)
-adj <- reshape(alledges[,-1], v.names = "tiepresent", idvar = "awdid", 
-               timevar = "personid", direction = "wide")
-adj[is.na(adj)] <- 0
+# Create 2-mode edgelist, then generate 2-mode network
+alledges <- read.csv("all_2mode.csv", header=T, as.is=T)  # note file location for your machine
+
+edges <- alledges[, c("awdid", "personid")]
+net <- graph_from_data_frame(edges, directed = F)
+V(net)$type <- bipartite.mapping(net)$type
 
 # Create node list with attributes
-allnodes <- read.csv("data/all_2mode nodes.csv", header=T, as.is=T)
-
-# Generate 2-mode network from the matrix above
-net <- graph_from_incidence_matrix(adj)
+allnodes <- read.csv("all_2mode nodes.csv", header=T, as.is=T)
 
 # Decompose the 2-mode award-person network to two one-mode networks; award-award (proj1) and person-person (proj2)
 net.bp <- bipartite.projection(net)
@@ -29,17 +27,15 @@ plot(pplenet, vertex.size=4, vertex.label=NA, layout=layout_with_fr)
 
 
 # GENERATE 2-MODE NETWORK FOR ONLY INVESTIGATORS THAT USED THE LIBRARY, 2016-2018
-# Create 2-mode edgelist, then reshape it to a matrix
-libuseredges <- read.csv("data/libuser_2mode.csv", header=T, as.is=T)
-adj2 <- reshape(libuseredges[,-1], v.names = "tiepresent", idvar = "awdid", 
-                timevar = "personid", direction = "wide")
-adj2[is.na(adj2)] <- 0
+# Create 2-mode edgelist, then generate 2-mode network
+libuseredges <- read.csv("libuser_2mode.csv", header=T, as.is=T)
+
+edges2 <- libuseredges[, c("awdid", "personid")]
+net2 <- graph_from_data_frame(edges2, directed = F)
+V(net2)$type <- bipartite.mapping(net2)$type
 
 # Create node list with attributes
-libusernodes <- read.csv("data/libuser_2mode nodes.csv", header=T, as.is=T)
-
-# Generate 2-mode network from the matrix above
-net2 <- graph_from_incidence_matrix(adj2)
+libusernodes <- read.csv("libuser_2mode nodes.csv", header=T, as.is=T)
 
 # Decompose the 2-mode award-person network to two one-mode networks
 net2.bp <- bipartite.projection(net2)
@@ -53,9 +49,9 @@ plot(pplenet2, vertex.size=4, vertex.label=NA, layout=layout_with_fr)
 
 # COMPARE THE TWO NETWORKS
 # Generate edge density...this runs quickly
-edge_density(pplenet, loops=F) # 0.0004684331
-edge_density(pplenet2, loops=F) # 0.0007458001
-0.0007458001/0.0004684331 # 1.592117
+edge_density(pplenet, loops=F) # 0.0003009092
+edge_density(pplenet2, loops=F) # 0.0004565804
+0.0004565804/0.0003009092 # 1.517336
 
 # EXPLORATORY COMMUNITY DETECTION IN THE TWO NETWORKS
 # Let's try an algorithm that runs relatively quickly...like greedy optimization of modularity
@@ -66,3 +62,4 @@ plot(clfastgr, vertex.size=4, vertex.label=NA, layout=layout_with_fr, as.undirec
 # library user investigators
 clfastgr2 <- cluster_fast_greedy(as.undirected(pplenet2))
 plot(clfastgr2, vertex.size=4, vertex.label=NA, layout=layout_with_fr, as.undirected(pplenet2))
+
